3f47cbd85e42f2e9ed59638f3f6c62613a72b606
Brian Munksgaard <brian.munksgaard@gmail.com>
2017-01-12 11:05:45 +0100

    EntityFramework: Model and DBContext created.

---
+++ EntityFramework/CodeFirst/CodeFirst/DAL/ExamContext.cs
@@ -0,0 +1,30 @@
+﻿using CodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.DAL
+{
+    /// <summary>
+    /// Database context for the application.
+    /// </summary>
+    public class ExamContext : DbContext
+    {
+        /// <summary>
+        /// Context constructor.
+        /// </summary>
+        public ExamContext() : base("ExamContext") { } // ExamContext in web.config.
+
+        /// <summary>
+        /// Students data.
+        /// </summary>
+        public DbSet<Student> Students { get; set; }
+        
+        /// <summary>
+        /// Courses data.
+        /// </summary>
+        public DbSet<Course> Courses { get; set; }
+    }
+}
