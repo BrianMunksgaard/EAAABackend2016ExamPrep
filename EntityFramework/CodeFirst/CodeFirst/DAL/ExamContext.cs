ba08b1d36ca83d8f8416ef837a9899982cdb96d9
Brian Munksgaard <brian.munksgaard@gmail.com>
2017-01-12 12:08:24 +0100

    DB seeding. MVC CRUD Controller/View.

--- EntityFramework/CodeFirst/CodeFirst/DAL/ExamContext.cs
+++ EntityFramework/CodeFirst/CodeFirst/DAL/ExamContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -26,5 +27,65 @@ namespace CodeFirst.DAL
         /// Courses data.
         /// </summary>
         public DbSet<Course> Courses { get; set; }
+
+        /// <summary>
+        /// Customize model creation.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Fluent API model customization.
+            /*
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Student>().ToTable("TheStudentsTable"); // Map entity to table.
+            */
+        }
+    }
+
+    /// <summary>
+    /// Database initializer.
+    /// </summary>
+    public class ExamContextInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ExamContext>
+    {
+        /// <summary>
+        /// Add information to the database.
+        /// </summary>
+        /// <param name="context"></param>
+        protected override void Seed(ExamContext context)
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student
+                {
+                    StudentName = "Brian Munksgaard"
+                },
+                new Student
+                {
+                    StudentName = "Jens Christian Rasch"
+                }
+            };
+
+            students.ForEach(s => context.Students.Add(s));
+            context.SaveChanges();
+
+            List<Course> courses = new List<Course>
+            {
+                new Course
+                {
+                    CourseName = "Backend"
+                },
+                new Course
+                {
+                    CourseName = "Frontend"
+                },
+                new Course
+                {
+                    CourseName = "Interfacedesign"
+                }
+            };
+
+            courses.ForEach(c => context.Courses.Add(c));
+            context.SaveChanges();
+        }
     }
 }
