acfb7dd88dd29915538585fe87d0914fdbcdaf39
Brian Munksgaard <brian.munksgaard@gmail.com>
2017-01-16 21:54:40 +0100

    First commit.

---
+++ RestAndWebApi/Code/CodeFirst/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.Models
+{
+    /// <summary>
+    /// This class is used to hold data about a course.
+    /// </summary>
+    public class Course
+    {
+        /// <summary>
+        /// Unique course id.
+        /// </summary>
+        public int CourseId { get; set; }
+
+        /// <summary>
+        /// The name of the course.
+        /// </summary>
+        public string CourseName { get; set; }
+    }
+}
