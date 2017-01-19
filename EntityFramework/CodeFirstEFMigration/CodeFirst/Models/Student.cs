7b51882d062004a9d2d527a1e6a0b2391cfef0a9
Brian Munksgaard <brian.munksgaard@gmail.com>
2017-01-19 14:06:37 +0100

    First commit.

---
+++ EntityFramework/CodeFirstEFMigration/CodeFirst/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.Models
+{
+    /// <summary>
+    /// This class is used to represent a student. In database the name will
+    /// be pluralized by convention so the table name is Students.
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Unique student id.
+        /// </summary>
+        //[Key] // Annotate as key if needed.
+        //[DatabaseGenerated(DatabaseGeneratedOption.None) // Avoid auto generated key if needed.
+        public int StudentId { get; set; } // Automatically recognized as primary key (Id or classnameId).
+
+        /// <summary>
+        /// The name of the student.
+        /// </summary>
+        public string StudentName { get; set; }
+    }
+}
